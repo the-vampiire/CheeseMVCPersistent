f1ae082d49d530791d2930bccaa036f934944cc2
The-Vampiire <vampiirecodes@gmail.com>
2018-03-20 05:14:13 -0400

    Completed the OneToMany (CheeseMVC persistent part two) studio

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -4,72 +4,79 @@ using System.Collections.Generic;
 using CheeseMVC.ViewModels;
 using CheeseMVC.Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace CheeseMVC.Controllers
-{
-    public class CheeseController : Controller
-    {
+// TODO: main index View have checkboxes for removing
+// POST to /remove
+
+// TODO: how about editing existing cheeses?
+
+namespace CheeseMVC.Controllers {
+    public class CheeseController : Controller {
         private readonly CheeseDbContext context;
 
-        public CheeseController(CheeseDbContext dbContext)
-        {
+        public CheeseController(CheeseDbContext dbContext) {
             context = dbContext;
         }
 
         // GET: /<controller>/
-        public IActionResult Index()
-        {
-            List<Cheese> cheeses = context.Cheeses.ToList();
-
+        public IActionResult Index() {
+            List<Cheese> cheeses = context.Cheeses.Include(cheese => cheese.Category).ToList();
             return View(cheeses);
         }
 
-        public IActionResult Add()
-        {
-            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
-            return View(addCheeseViewModel);
-        }
+        /**
+         *
+         * this is a shorthand equivalent to
+         *
+         * public IActionResult Add() = {
+         *  AddCheeseViewModel addCheeseViewModel = new AddCheseViewModel(context.Categories.ToList());
+         *  return View(addCheeseViewModel);
+         * }
+         * 
+         */
+        public IActionResult Add() => View(new AddCheeseViewModel(context.Categories.ToList()));
 
         [HttpPost]
-        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                // Add the new cheese to my existing cheeses
-                Cheese newCheese = new Cheese
-                {
-                    Name = addCheeseViewModel.Name,
-                    Description = addCheeseViewModel.Description,
-                    Type = addCheeseViewModel.Type
-                };
-
-                context.Cheeses.Add(newCheese);
-                context.SaveChanges();
-
-                return Redirect("/Cheese");
-            }
-
-            return View(addCheeseViewModel);
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel) {
+            if (!ModelState.IsValid) return View(addCheeseViewModel);
+           
+            context.Cheeses.Add(new Cheese {
+                Name = addCheeseViewModel.Name,
+                Description = addCheeseViewModel.Description,
+                CategoryID = addCheeseViewModel.CategoryID
+            });
+            context.SaveChanges();
+            return Redirect("/Cheese");
         }
 
-        public IActionResult Remove()
-        {
+        public IActionResult Remove() {
             ViewBag.title = "Remove Cheeses";
             ViewBag.cheeses = context.Cheeses.ToList();
             return View();
         }
 
         [HttpPost]
-        public IActionResult Remove(int[] cheeseIds)
-        {
-            foreach (int cheeseId in cheeseIds)
-            {
+        public IActionResult Remove(int[] cheeseIds) {
+            foreach (int cheeseId in cheeseIds) {
                 Cheese theCheese = context.Cheeses.Single(c => c.ID == cheeseId);
                 context.Cheeses.Remove(theCheese);
             }
+            
+            /*
+             alternative approach
+             RemoveRange takes an iterable (a List in this case) of Cheese objects to remove
+             we use the Linq iterable Select() method to transform [mutate] the array into a new form
+             see AddCheeseViewModel.cs for more notes on Select()
+             
+             context.Cheeses.RemoveRange(
+                cheeseIds.Select(
+                    id => context.Cheeses.Single(x => x.ID == id)
+                ).ToList()        
+             );
+            */
 
             context.SaveChanges();
-
             return Redirect("/");
         }
     }
