76c6b86cb3cf57cf546b9fb5f9c61f2263d1c110
The-Vampiire <vampiirecodes@gmail.com>
2018-03-20 23:06:28 -0400

    completed studio part 2 (one to many) with notes

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -9,17 +9,12 @@ namespace CheeseMVC.Models
         public int ID { get; set; }
         
         // this is a FOREIGN KEY [FK]
-            // the ID of the category is stored ON the Cheese table
-            // a Cheese HAS ONE Category 
+            // the ID of the category is stored in rows of Cheese table because a Cheese HAS ONE Category 
             // meaning a Category HAS MANY Cheeses
                 // a ONE to MANY relationship constrained by a foreign key
         public int CategoryID { get; set; }
         
         // the CheeseType enum is replaced by data from the Category table identified by the CategoryID FK
         public CheeseCategory Category { get; set; }
-        
-// TODO: notes on how this relates to the SQL table structure / relationships
-        // BUG: ??represents a list of Cheeses related to a particular Category??
-        public IList<Cheese> Cheeses { get; set; }
     }
 }
--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -22,30 +22,33 @@ namespace CheeseMVC.ViewModels
         public int CategoryID { get; set; }
         public List<SelectListItem> Categories { get; set; }
 
+        // constructor that accepts a categories argument from the DbContext in the controller
         public AddCheeseViewModel(IEnumerable<CheeseCategory> categories) {
-
-            Categories = categories.ToList();
-
-            // <option value="0">Hard</option>
-            CheeseTypes.Add(new SelectListItem {
-                Value = ((int) CheeseType.Hard).ToString(),
-                Text = CheeseType.Hard.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Soft).ToString(),
-                Text = CheeseType.Soft.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Fake).ToString(),
-                Text = CheeseType.Fake.ToString()
-            });
+            
+            /**
+             * for those familiar with the Array.map() method in javascript look here:
+             * https://stackoverflow.com/questions/32959468/example-of-array-map-in-c
+             *
+             * this is known as a Select method in C# (from the Linq List methods) 
+             * what the Select method does is loop over the contents of a List (or other iterable, also referre)
+             * and produce a new List.
+             * each element in the original list has some function or mutation applied to it
+             * the new list that is produced now contains elements that have been "mapped" or mutated
+             *
+             * this is a convenient way of applying some transformation of data on all elements in an iterable
+             */
+            Categories = categories.Select(category =>
+                new SelectListItem {
+                    Value = category.ID.ToString(),
+                    Text = category.Name
+                }
+            ).ToList(); 
 
         }
-        
-        public AddCheeseViewModel()
+        // default constructor for model binding
+        // we pass in an emtpy "shell" AddCheeseViewModel to store the form contents
+        // then we use this shell (temporary storage) to set the properties of a new model
+        // in the POST request handler for that route
+        public AddCheeseViewModel() {}
     }
 }
